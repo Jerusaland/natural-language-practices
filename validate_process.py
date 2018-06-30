17eed4b9fb3cc1160790350eeff9dbe2b5692132
Jerusaland <campzhai@126.com>
2018-06-30 23:01:59 +0800

    update the validate set from Chinese words to Chinese Pinyin

--- validate_process.py
+++ validate_process.py
@@ -1,14 +1,18 @@
+# -*- coding: utf-8 -*-
+
 import os
 import time
 import numpy as np
 import tensorflow as tf
+from dataset_handling import *
+from neural_networks import *
 
 
 #define the validation processes
 def validate_process():
     
     batch_size = 1
-    mfcc_dims = 20
+    mfcc_dims = 13
 
     wav_path = 'd:\practices\STT_Dataset\test'
     text_path = 'd:\practices\STT_Dataset\data'
@@ -16,9 +20,9 @@ def validate_process():
     #get the validation wav file list and corresponding texts
     wav_files,true_texts, _ = get_wav_and_text(wav_path, text_path)
 	
-	#get words bag
-	words,_,_,_,_ = get_text_features(true_texts)
-	words_quantity = len(words) # blank space included
+	#get true labels
+	_,_,_,pinyin_num_dict,output_dims = get_text_features(true_texts)
+	all_pinyin = sorted(pinyin_num_dict.keys())
 
     graph = tf.Graph()
     with graph.as_default():    
@@ -28,11 +32,11 @@ def validate_process():
 		seq_len = tf.placeholder(dtype=tf.int32)
     
 		#get neural network output - using placeholder
-		logits = bi_gru_model(X, batch_size, words_quantity)
+		logits = bi_gru_model(X, batch_size, output_dims)
 		logits = tf.transpose(logits, perm=[1, 0, 2])
 	
         #CTC decode for computing training error
-        decoded, _ = tf.nn.ctc_greedy_decoder(logits, seq_len)
+        decoded, _ = tf.nn.ctc_beam_search_decoder(logits, seq_len)
  	
     saver = tf.train.Saver()    
     with tf.Session() as sess:
@@ -46,12 +50,12 @@ def validate_process():
 			d = session.run(decoded[0], feed_dict=feed)
 
 			dense_decoded = tf.sparse_tensor_to_dense(d, default_value=-1).eval(session=sess)
-            dense_labels = utils.trans_sparse_to_texts(sparse_tensor, words)
+            dense_labels = utils.trans_sparse_to_texts(sparse_tensor, all_pinyin)
         
             for orig, decoded_array in zip(dense_labels, dense_decoded):
                 # 转成string
-                decoded_str = utils.trans_array_to_text_ch(decoded_array, words)
-                print('语音原始文本: {}'.format(orig))
-                print('识别出来的文本:  {}'.format(decoded_str))
+                decoded_str = utils.trans_array_to_text_ch(decoded_array, all_pinyin)
+                print('语音原始文本拼音: {}'.format(orig))
+                print('识别出来的文本拼音:  {}'.format(decoded_str))
 				print('\n')
 
